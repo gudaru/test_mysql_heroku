cb3c7bbd412b4d09ea5308261dca7795794d92dc
gudaru <nguyendangluc91@gmail.com>
2013-07-02 10:13:41 +0700

    1 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMysqlHeroku::Application.config.secret_token = '0ace4ca76b4d67b801fd71dbff9dcb2e0114bded7ffa0d63f88e93507bcc77699cdf473409e8ea4eae3ec253b648d01b1938d2efce2d02c6f2aaa5579f66ae92'
